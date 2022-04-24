2d5a790b34531bae747c4df8e8aa264700579846
imRajAryan09 <raj.12020162@lpu.in>
2022-04-25 02:02:10 +0530

    hero section done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import "./navbar.scss";
+import PatternIcon from "@mui/icons-material/Pattern";
 const Navbar = () => {
 	return (
 		<nav>
-			<div className="logo">PassGen</div>
+			<div className="logo">
+				<PatternIcon className="logo-icon" />
+				PassGen
+			</div>
 			<div className="nav-items">
 				<ul>
 					<li>Home</li>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,24 @@
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
+import BgSvg from "../../assets/hero-svg-pwdgen.png";
 const Home = () => {
 	return (
 		<div className="home">
 			<div className="nav-component">
 				<Navbar />
+			</div>
+			<div className="home-content">
+				<div className="text">
+					<h1>Keep Your Private Data Secure</h1>
+					<p>Generate secure and strong passwords with ease. </p>
+					<div className="button">Generate Password</div>
+				</div>
+				<div className="image">
+					<img
+						src={BgSvg}
+						alt="hero-img"
+					/>
+				</div>
 			</div>
 		</div>
 	);
