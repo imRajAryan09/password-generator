969c688acf12b256f16b91af5d94f04f90033481
imRajAryan09 <raj.12020162@lpu.in>
2022-05-10 02:41:09 +0530

    button beautified

---
+++ src/components/lock/Lock.jsx
@@ -0,0 +1,13 @@
+import "./lock.scss";
+
+const Lock = () => {
+	return (
+		<div class="lock-container">
+			<div class="lock">
+				<div class="keyhole"></div>
+			</div>
+		</div>
+	);
+};
+
+export default Lock;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,23 +1,26 @@
 import Navbar from "../../components/navbar/Navbar";
+// import Announcement from "../../components/announcement/Announcement";
 import "./home.scss";
 import BgSvg from "../../assets/hero-svg-pwdgen.png";
+import Lock from "../../components/lock/Lock";
 const Home = () => {
 	return (
 		<div className="home">
 			<div className="nav-component">
+				{/* <Announcement/> */}
 				<Navbar />
 			</div>
 			<div className="home-content">
 				<div className="text">
 					<h1>Keep Your Private Data Secure</h1>
 					<p>Generate secure and strong passwords with ease. </p>
-					<div className="button">Generate Password</div>
+					<div className="button">
+						<Lock className="lock"/>
+						<span>Generate Password</span>
+					</div>
 				</div>
 				<div className="image">
-					<img
-						src={BgSvg}
-						alt="hero-img"
-					/>
+					<img src={BgSvg} alt="hero-img" />
 				</div>
 			</div>
 		</div>
