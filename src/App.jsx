825c7f435ad0802f02f46d1b862b8abb3f212656
imRajAryan09 <raj.12020162@lpu.in>
2022-05-17 02:12:20 +0530

    fallback route set

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Generate from "./pages/generate/Generate";
@@ -10,6 +10,7 @@ function App() {
 				<Route index element={<Home />} />
 				<Route path="/generate" element={<Generate />} />
 				<Route path="/about" element={<About />} />
+				<Route path="*" element={<Navigate to="/" />} />
 			</Routes>
 		</div>
 	);
